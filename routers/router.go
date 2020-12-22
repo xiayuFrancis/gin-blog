661fc87b295cce1ccf28bc594897849150c24bc6
xiayu <809700565@qq.com>
2020-12-22 14:57:27 +0800

    jwt

--- routers/router.go
+++ routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"gin-blog/middleware/jwt"
 	"gin-blog/pkg/setting"
+	"gin-blog/routers/api"
 	v1 "gin-blog/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -12,7 +14,9 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth", api.GetAuth)
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		apiv1.GET("/tags", v1.GetTags)
 		apiv1.POST("/tags", v1.AddTags)
